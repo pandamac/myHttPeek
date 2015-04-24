8e796def57ce8d78c334d06cff6c2d0f79c19cf1
zcc1414 <panda@MacBook-Pro.local>
2015-04-24 09:49:39 +0800

    init root App

---
+++ myhttpeek/RootViewController.mm
@@ -0,0 +1,21 @@
+#import "RootViewController.h"
+
+@implementation RootViewController
+
+- (void)reboot
+{
+    NSLog(@"iOSREDebug: %d, %d, %d", getuid(), geteuid(), system("reboot"));
+}
+
+- (void)loadView {
+	self.view = [[[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]] autorelease];
+	self.view.backgroundColor = [UIColor redColor];
+    
+    UIButton *rebootButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    rebootButton.frame = CGRectMake(0.0f, 0.0f, 60.0f, 60.0f);
+    rebootButton.center = self.view.center;
+    rebootButton.backgroundColor = [UIColor yellowColor];
+    [rebootButton addTarget:self action:@selector(reboot) forControlEvents:UIControlEventTouchDown];
+    [self.view addSubview:rebootButton];
+}
+@end
