4184c390a5bfe8fe67d625ea1ed34bfebd8d4b3e
zcc1414 <pandashellcode@163.com>
2016-01-27 10:00:43 +0800

    httpover

---
+++ httpeekdylib/httpeekdylib/Sources/IFHOOK.h
@@ -0,0 +1,37 @@
+//
+//  IFHOOK.h
+//  FileMonitor
+//
+//  Created by panda on 17/12/15.
+//
+//
+
+#define NSLog(...)
+
+#define NSFileHandle_IF_HOOK
+
+#define NSData_IF_HOOK
+
+#define NSArray_IF_HOOK
+
+#define NSDictionary_IF_HOOK
+
+#define NSJSONSerialization_IF_HOOK
+
+#define JSONKit_IF_HOOK
+
+#define NSString_IF_HOOK
+
+#define NSBundle_IF_HOOK
+
+#define NSFileManager_IF_HOOK
+
+#define Keychainity_IF_HOOK
+
+#define sqlite3_IF_HOOK
+
+#define NSUserDefaults_IF_HOOK
+
+#define HASH_IF_HOOK
+
+#define Crypto_IF_HOOK
---
+++ testMKNetWorkKi/testMKNetWorkKi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testMKNetWorkKi
+//
+//  Created by panda on 19/11/15.
+//  Copyright (c) 2015年 360. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
